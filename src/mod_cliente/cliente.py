cbdd633c8a0f92ce4bda219ebfa712f144a17f7f
Felipe Guimaraes <felipeguisc@gmail.com>
2023-11-12 09:01:19 -0500

    Edit added to Funcionario and Cliente

--- src/mod_cliente/cliente.py
+++ src/mod_cliente/cliente.py
@@ -26,21 +26,17 @@ def formCliente():
 @bp_cliente.route('/insert', methods=['POST'])
 def insert():
     try:
-        print('Começou insert Cliente')
         # dados enviados via FORM
         nome = request.form['nome']
         cpf = request.form['cpf']
         telefone = request.form['telefone']
-        print('Começou parse diaFiado')
         compraFiado = request.form['compraFiado']
-        print('Começou parse diaFiado')
         diaFiado_string = request.form['diaFiado']
         # Convert the string to bytes using UTF-8 encoding
         diaFiado_bytes = diaFiado_string.encode('utf-8').hex()
         
         #senha = request.form['senha’]
         senha = Funcoes.cifraSenha(request.form['senha'])
-        print('COmeçou monta payload')
         # monta o JSON para envio a API
         payload = {'id': 0, 
                    'nome': nome, 
@@ -49,14 +45,6 @@ def insert():
                    'compraFiado': compraFiado, 
                    'diaFiado' : diaFiado_bytes, 
                    'senha': senha}
-        
-        print('id: ', 0, 
-                   'nome: ', nome, 
-                   'cpf: ', cpf, 
-                   'telefone: ', telefone, 
-                   'compraFiado: ', compraFiado, 
-                   'diaFiado; ', diaFiado_bytes, 
-                   'senha: ', senha )
         
         # executa o verbo POST da API e armazena seu retorno
         response = requests.post(ENDPOINT_CLIENTE, headers=HEADERS_API, json=payload, verify=False)
@@ -69,4 +57,62 @@ def insert():
 
         return redirect(url_for('cliente.formListaCliente', msg=result[0]))
     except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route("/form-edit-cliente", methods=['POST'])
+def formEditCliente():
+    try:
+        # ID enviado via FORM
+        id_cliente = request.form['id']
+        # executa o verbo GET da API buscando somente o cliente selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_CLIENTE + id_cliente, headers=HEADERS_API, verify=False)
+        print(response)
+        result = response.json()
+        print(result)
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        # renderiza o form passando os dados retornados
+        return render_template('formCliente.html', result=result)
+    
+    except Exception as e:
+        print(e.args[0])
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route('/edit', methods=['POST'])
+def edit():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        compraFiado = request.form['compraFiado']
+        diaFiado_string = request.form['diaFiado']
+        # Convert the string to bytes using UTF-8 encoding
+        diaFiado_bytes = diaFiado_string.encode('utf-8').hex()
+        #senha = request.form['senha’]
+        senha = Funcoes.cifraSenha(request.form['senha'])
+        
+        # monta o JSON para envio a API
+        payload = {'id': id_cliente, 
+                   'nome': nome, 
+                   'cpf': cpf, 
+                   'telefone': telefone, 
+                   'compraFiado': compraFiado, 
+                   'diaFiado' : diaFiado_bytes,
+                   'senha': senha}
+        
+        print(payload)
+        
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(ENDPOINT_CLIENTE, headers=HEADERS_API, json=payload, verify=False)
+        result = response.json()
+        print(result)
+        
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+    except Exception as e:
+        print(e.args[0])
         return render_template('formListaCliente.html', msgErro=e.args[0])
--- src/mod_funcionario/funcionario.py
+++ src/mod_funcionario/funcionario.py
@@ -51,3 +51,50 @@ def insert():
     
     except Exception as e:
         return redirect(url_for('funcionario.formListaFuncionario', msgErro=e.args[0]))
+    
+@bp_funcionario.route("/form-edit-funcionario", methods=['POST'])
+def formEditFuncionario():
+    try:
+        # ID enviado via FORM
+        id_funcionario = request.form['id']
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_FUNCIONARIO + id_funcionario, headers=HEADERS_API, verify=False)
+        print(response)
+        result = response.json()
+        print(result)
+        if (response.status_code != 200):
+            raise Exception(result[0])
+        # renderiza o form passando os dados retornados
+        return render_template('formFuncionario.html', result=result)
+    
+    except Exception as e:
+        print(e.args[0])
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+    
+@bp_funcionario.route('/edit', methods=['POST'])
+def edit():
+    try:
+        # dados enviados via FORM
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+        
+        # monta o JSON para envio a API
+        payload = {'id': id_funcionario, 'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo':
+        grupo, 'senha': senha}
+        
+        # executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(ENDPOINT_FUNCIONARIO, headers=HEADERS_API, json=payload, verify=False)
+        result = response.json()
+        
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+    except Exception as e:
+        print(e.args[0])
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
