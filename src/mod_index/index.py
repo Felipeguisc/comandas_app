792f8206de42bc6a232463dbaf1a4bfde72ba446
Felipe Guimaraes <felipeguisc@gmail.com>
2023-11-26 17:37:27 -0500

    Added config and static login flow

--- src/mod_index/index.py
+++ src/mod_index/index.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 
-bp_index = Blueprint('index', __name__, url_prefix="/", template_folder='templates')
+bp_index = Blueprint('index', __name__, url_prefix="/home",
+template_folder='templates')
 
-@bp_index.route('/')
+@bp_index.route('/') 
 def formIndex():
     return render_template('formIndex.html'), 200
